2fbcad339276918a629ce894db6ba62a9529089d
sohan380 <sohanmupparaju@gmail.com>
2023-12-15 23:57:56 +0530

    1.2

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,13 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.patches import Patch
 
 def BubbleSort(arr):
   n = len(arr)
   x = np.arange(0, n, 1)
   swapped = False
 
-  # Create the plot outside the inner loop
+  # Create the plot
   fig, ax = plt.subplots()
   bars = ax.bar(x, arr)
   ax.set_title('Bubble Sort Visualization')
@@ -15,14 +14,15 @@ def BubbleSort(arr):
   ax.set_ylabel('Value')
   ax.set_xticks(x)
 
+  # Set low for more speed
+  spd = 0.05
+
   for i in range(n-1):
     for j in range(n-i-1):
-      # Update data
-      bars[j].set_height(arr[j])
-      bars[j+1].set_height(arr[j+1])
-
-      # Increase for slowing
-      plt.pause(0.01)
+      # Green for smaller, Red for larger and Orange for sorted.
+      bars[j].set_color('green')
+      bars[j+1].set_color('red')
+      plt.pause(spd)
 
       if arr[j] > arr[j+1]:
         swapped = True
@@ -30,9 +30,22 @@ def BubbleSort(arr):
         arr[j], arr[j+1] = arr[j+1], arr[j]
         bars[j].set_height(arr[j])
         bars[j+1].set_height(arr[j+1])
+        plt.pause(spd)
+        bars[j].set_color('green')
+        bars[j+1].set_color('red')
+
+      plt.pause(spd)
+      bars[j].set_color('#1f77b4')
+
+    bars[n-i-1].set_color('orange')
+
+    if i==n-2:
+      bars[0].set_color('orange')
 
     # Check if any swaps occurred and break if not
     if not swapped:
+      for t in range(n):
+        bars[t].set_color('orange')
       break
 
   for i, value in enumerate(arr):
@@ -43,5 +56,6 @@ def BubbleSort(arr):
 arr = np.random.randint(0, 100, 15)
 # arr = [1,2,3,4,5,6,7,8,9,10]
 # arr = [10,9,8,7,6,5,4,3,2,1]
-print(arr)
+print("Input: ",arr)
 BubbleSort(arr)
+print("Output: ",arr)
---
+++ SelectionSort.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def SelectionSort(arr):
+   n = len(arr)
+   x = np.arange(0, n, 1)
+
+   # Create the plot
+   fig, ax = plt.subplots()
+   bars = ax.bar(x, arr)
+   ax.set_title('Selection Sort Visualization')
+   ax.set_xlabel('Index')
+   ax.set_ylabel('Value')
+   ax.set_xticks(x)
+
+   # Set speed
+   spd = 0.05
+   
+   for i in range(n):
+        # Index of least element
+        li = i
+        bars[li].set_color('green')
+        for j in range(i+1, n):
+            bars[j].set_color('red')
+            plt.pause(spd)
+            if arr[j] < arr[li]:
+                bars[li].set_color('#1f77b4')
+                li = j
+                bars[li].set_color('green')
+                plt.pause(spd)
+
+            else:
+                bars[j].set_color('#1f77b4')
+                # plt.pause(spd)
+
+            if j==n-1:
+                bars[li].set_color('#1f77b4')
+                # plt.pause(spd)
+        
+        # Swap minimum
+        arr[i], arr[li] = arr[li], arr[i]
+        bars[i].set_height(arr[i])
+        bars[li].set_height(arr[li])
+        bars[i].set_color('orange')
+        plt.pause(spd)
+
+   plt.show()
+        
+arr = np.random.randint(0, 100, 15)
+# arr = [1,2,3,4,5,6,7,8,9,10]
+# arr = [10,9,8,7,6,5,4,3,2,1]
+print("Input: ",arr)
+SelectionSort(arr)
+print("Output: ",arr)
